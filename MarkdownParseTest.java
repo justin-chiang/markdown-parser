1e1812c0c792208ab7a846114d73f0e985f8e3be
justin-chiang <59491073+justin-chiang@users.noreply.github.com>
2023-05-22 22:07:49 -0700

    added to repo

---
+++ MarkdownParseTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.junit.*;
+public class MarkdownParseTest {
+
+    @Test
+    public void newFile2Test() throws IOException{
+        String markdown = Files.readString(Path.of("new-file-2.md"));
+        assertEquals(List.of(), MarkdownParse.getLinks(markdown));
+    }
+
+    @Test
+    public void newFile5Test() throws IOException{
+        String markdown = Files.readString(Path.of("new-file-5.md"));
+        assertEquals(List.of(), MarkdownParse.getLinks(markdown));
+    }
+
+}
